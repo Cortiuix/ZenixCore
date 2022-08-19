795afa451053d3824e6cb1c978b1ba3e503edf01
Cortiuix <cortiuix.dev@gmail.com>
2022-08-19 17:17:28 -0500

    Prueba rec

---
+++ slashcommands/general/recommendation.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder} = require('@discordjs/builders')
+const { MessageEmbed } = require('discord.js')
+const Discord = require('discord.js')
+const warndb = require('../../models/warn-model.js')
+module.exports = {
+
+    data: new SlashCommandBuilder()
+
+    .setName('recommendation')
+    .setDescription('send a recommendation to the bot owner')
+    .addStringOption(option =>
+        option
+        .setName("text")
+        .setDescription("write your recommendation")
+        .setRequired(true)
+        ),
+
+        /**
+         * @param {import{'discord.js'}.Interaction} int
+         */  
+
+    async run(client, int){
+
+        const user = ["903037096086888508"]
+
+        const texto = int.options.getString("text")
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle("Nueva Recomendación")
+        .setDescription(`Usuario: **${int.user.tag}** \n Recomendación: `${texto}``)
+        .setColor("YELLOW")
+
+        user.send({ embeds: [embed] })
+
+
+    }
+}
