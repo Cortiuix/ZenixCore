0b11e2a946cbd16e285556b8f709b9367e299a1d
Cortiuix <cortiuix.dev@gmail.com>
2022-11-15 20:06:19 -0600

    Hel command z!

--- comandos/util/help.js
+++ comandos/util/help.js
@@ -10,7 +10,7 @@ module.exports = {
   .setTitle("Command List!")
   .setColor("#3e75a5")           
   .setThumbnail("https://cdn.discordapp.com/avatars/778684218938097704/f2db95f18d9e16d1417884d0ec23211c.webp?size=1024")
-  .setDescription("**Utility Commands** \n\n ``p!warn, p!warn-remove, p!warns, p!help`` \n\n **Moderation Commands** \n\n ``p!ban, p!kick, p!nuke`` \n\n **Fun Commands** \n\n ``p!avatar`` \n\n **Music Commands** \n\n  ``p!play, p!pause, p!continue, p!queue, p!skip, p!stop, p!volume`` \n\n **Accion commands**  \n\n ``p!gay, p!hug, p!pat, p!punch`` \n\n **General Commands** \n\n ``p!calculator`` \n\n **Economy Commands** \n\n ``p!work, p!with, p!shop, p!setitems, p!sell, p!rob, p!remove-item, p!leaderboard, p!inventory, p!give, p!gamble, p!dep, p!daily, p!buy, p!bal, p!add-item``")
+  .setDescription("**Utility Commands** \n\n ``z!warn, z!warn-remove, z!warns, z!help`` \n\n **Moderation Commands** \n\n ``z!ban, z!kick, z!nuke`` \n\n **Fun Commands** \n\n ``z!avatar`` \n\n **Music Commands** \n\n  ``z!play, z!pause, z!continue, z!queue, z!skip, z!stop, z!volume`` \n\n **Accion commands**  \n\n ``z!gay, z!hug, z!pat, z!punch`` \n\n **General Commands** \n\n ``z!calculator`` \n\n **Economy Commands** \n\n ``z!work, z!with, z!shop, z!setitems, z!sell, z!rob, z!remove-item, z!leaderboard, z!inventory, z!give, z!gamble, z!dep, z!daily, z!buy, z!bal, z!add-item``")
 
     message.channel.send({ embeds: [embed] })
   
