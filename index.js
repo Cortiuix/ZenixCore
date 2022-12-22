d56d4f01f4db81978baa1cab23fc72a4ffed98c8
Cortiuix <82616578+Cortiuix@users.noreply.github.com>
2022-12-22 13:45:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -155,9 +155,6 @@ for(const file of fs.readdirSync('./distube/')){
   }
 }
 
-const express = require('express')
-const app = express()
-
 app.get('/invite', (req, res) => {
   res.redirect("https://discord.com/api/oauth2/authorize?client_id=778684218938097704&permissions=8&scope=bot%20applications.commands")
 })
